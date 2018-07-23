0cbf0b6d099fe74ead3a6ece4c8651a04d618c46
Mike Howlett <mah9882@rit.edu>
2018-07-23 15:13:45 -0400

    First commit; Uploading Unreal Project to source control.

---
+++ Source/AlsEscape/AlsEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlsEscapeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAlsEscapeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAlsEscapeGameMode();
+};
+
+
+
